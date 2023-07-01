cdaa2595aeb2c86477450f30fb7d00e25ad761fb
fenroe <scrambles9494@gmail.com>
2023-07-01 03:33:11 +0300

    feat:transcribed menu data

---
+++ client/src/data/menuData.js
@@ -0,0 +1,652 @@
+export const menuData = {
+    starters: [
+        {
+            id: 1,
+            name: "Thai Meatballs",
+            nimi: "Thai Lihapullat",
+            spice: 1,
+            price: 7.00
+        },
+        {
+            id: 2,
+            name: "Papaya Salad",
+            nimi: "Papaijasalaatti (Som Tam)",
+            spice: 2,
+            price: 7.00
+        },
+        {
+            id: 3,
+            name: "Spring Rolls",
+            nimi: "Kevätkaaryeet",
+            spice: 0,
+            price: 6.00
+        },
+        {
+            id: 4,
+            name: "Chicken Satay",
+            nimi: "Kanaa Satay",
+            spice: 0,
+            price: 6.00
+        },
+        {
+            id: 5,
+            name: "Thung Tong (Vegetarian)",
+            nimi: "Thung Tong (Kasvis)",
+            spice: 0,
+            price: 7.00
+        },
+        {
+            id: 6,
+            name: "Tom Kha Chicken Soup",
+            nimi: "Tom Kha - Kanakeitto",
+            spice: 1,
+            price: 7.00
+        },
+        {
+            id: 7,
+            name: "Tom Yam Seafood Soup",
+            nimi: "Tom Yam - Äyriäskeitto",
+            spice: 2,
+            price: 7.00
+        },
+        {
+            id: 8,
+            name: "Deep Fried Shrimps",
+            nimi: "Friteeratut Katkaravut",
+            spice: 0,
+            price: 6.50
+        }
+    ],
+    tofu: [
+        {
+            id: 9,
+            name: "Fried Vegetables with Tofu",
+            nimi: "Paistettuja Vihanneksia Tofulla",
+            spice: 0,
+            price: 13.90
+        },
+        {
+            id: 10,
+            name: "Fried Tofu with Chili Sauce",
+            nimi: "Paistettua Tofua Chilikastikkeessa",
+            spice: 1,
+            price: 13.90
+        },
+        {
+            id: 11,
+            name: "Fried Tofu with Fresh Ginger",
+            nimi: "Paistettua Tofua Tuoreella Inkiväärillä",
+            spice: 1,
+            price: 13.90
+        },
+        {
+            id: 12,
+            name: "Fried Tofu with Cashew Nuts",
+            nimi: "Paistettua Tofua Cashewpähkinöillä",
+            spice: 1,
+            price: 13.90
+        },
+        {
+            id: 13,
+            name: "Tofu with Panang Curry",
+            nimi: "Tofua Panang Curryssa",
+            spice: 1,
+            price: 13.90
+        },
+        {
+            id: 14,
+            name: "Tofu with Red Curry",
+            nimi: "Tofua Punacurryssa",
+            spice: 2,
+            price: 13.90
+        },
+        {
+            id: 15,
+            name: "Tofu with Green Curry",
+            nimi: "Tofua Vihercurryssa",
+            spice: 2,
+            price: 13.90
+        },
+        {
+            id: 16,
+            name: "Tofu with Yellow Curry",
+            nimi: "Tofua Keltacurryssa",
+            spice: 1,
+            price: 13.90
+        },
+        {
+            id: 17,
+            name: "Tofu with Massaman Curry",
+            nimi: "Tofua Massaman Curryssa",
+            spice: 1,
+            price: 13.90
+        },
+        {
+            id: 18,
+            name: "Fried Noodles with Tofu (Pad Thai)",
+            nimi: "Paistettua Nuudelia, Tofua ja Vihanneksia",
+            spice: 0,
+            price: 13.90
+        },
+    ],
+    chicken: [
+        {
+            id: 19,
+            name: "Fried Chicken with Vegetables",
+            nimi: "Paistettua Kanaa ja Vihanneksia",
+            spice: 0,
+            price: 14.90
+        },
+        {
+            id: 20,
+            name: "Fried Chicken with Garlic",
+            nimi: "Paistettua Kanaa Valkosipulissa",
+            spice: 0,
+            price: 14.90
+        },
+        {
+            id: 21,
+            name: "Fried Chicken with Sweet and Sour Sauce",
+            nimi: "Paistettua Kanna Hapanimeläkastikkeessa",
+            spice: 0,
+            price: 14.90
+        },
+        {
+            id: 22,
+            name: "Fried Chicken with Yellow Curry",
+            nimi: "Paistettua Kanaa Keltacurryssa",
+            spice: 1,
+            price: 14.90
+        },
+        {
+            id: 23,
+            name: "Fried Chicken with Cashew Nuts",
+            nimi: "Paistettua Kanaa Cashewpähkinöillä",
+            spice: 1,
+            price: 14.90
+        },
+        {
+            id: 24,
+            name: "Fried Chicken with Fresh Ginger",
+            nimi: "Paistettua Kanaa Tuoreella Inkiväärillä",
+            spice: 1,
+            price: 14.90
+        },
+        {
+            id: 25,
+            name: "Chicken with Basil Leaves",
+            nimi: "Kana ja Basilikaa",
+            spice: 2,
+            price: 14.90
+        },
+        {
+            id: 26,
+            name: "Fried Chicken with Chili Sauce",
+            nimi: "Paistettua Kanaa Chilikastikkeessa",
+            spice: 1,
+            price: 14.90
+        },
+        {
+            id: 27,
+            name: "Chicken with Massaman Curry",
+            nimi: "Kanaa Massaman Curryssa",
+            spice: 1,
+            price: 14.90
+        },
+        {
+            id: 28,
+            name: "Chicken with Panang Curry",
+            nimi: "Kanaa Panang Curryssa",
+            spice: 1,
+            price: 14.90
+        },
+        {
+            id: 29,
+            name: "Chicken with Red Curry",
+            nimi: "Kanaa Punacurry Kookosmaidossa",
+            spice: 2,
+            price: 14.90
+        },
+        {
+            id: 30,
+            name: "Chicken with Green Curry",
+            nimi: "Kanaa Vihercurry Kookosmaidossa",
+            spice: 2,
+            price: 14.90
+        },
+        {
+            id: 31,
+            name: "Chicken with Yellow Curry",
+            nimi: "Kanna Keltacurry Kookosmaidossa",
+            spice: 1,
+            price: 14.90
+        },
+        {
+            id: 32,
+            name: "Chicken with Keang Kuo (Red Curry)",
+            nimi: "Kanaa Keang Kuo (Punacurry)",
+            spice: 2,
+            price: 14.90
+        },
+        {
+            id: 33,
+            name: "Chicken Tom Yam Soup",
+            nimi: "Kanaa Tom Yam-keitto",
+            spice: 2,
+            price: 14.90
+        },
+        {
+            id: 34,
+            name: "Chicken Tom Kha Soup",
+            nimi: "Kanaa Tom Kha-keitto",
+            spice: 1,
+            price: 14.90
+        },
+        {
+            id: 90, // sic ? I'm transcribing the menu verbatim
+            name: "Chicken with Sweet Chili Paste",
+            nimi: "Kanaa Makeassa Chilikastikkeessa",
+            spice: 2,
+            price: 14.90
+        }
+    ],
+    pork: [
+        {
+            id: 35,
+            name: "Fried Pork with Vegetables",
+            nimi: "Paistettua Porsasta ja Vihanneksia",
+            spice: 0,
+            price: 14.90
+        },
+        {
+            id: 36,
+            name: "Fried Pork with Garlic",
+            nimi: "Paistettua Porsasta Valkosipulissa",
+            spice: 0,
+            price: 14.90
+        },
+        {
+            id: 37,
+            name: "Fried Pork with Sweet and Sour Sauce",
+            nimi: "Paistettua Porsasta Hapanimeläkastikkeessa",
+            spice: 0,
+            price: 14.90
+        },
+        {
+            id: 38,
+            name: "Fried Pork with Cashew Nuts",
+            nimi: "Paistettua Porsasta Cashewpähkinöillä",
+            spice: 1,
+            price: 14.90
+        },
+        {
+            id: 39,
+            name: "Fried Pork with Fresh Ginger",
+            nimi: "Paistettua Porsasta Tuoreella inkiväärillä",
+            spice: 1,
+            price: 14.90
+        },
+        {
+            id: 40,
+            name: "Fried Pork and Basil Leaves",
+            nimi: "Paistettua Porsasta ja Basilikaa",
+            spice: 2,
+            price: 14.90
+        },
+        {
+            id: 41,
+            name: "Pork with Massaman Curry",
+            nimi: "Porsasta Massaman Curryssa",
+            spice: 1,
+            price: 14.90
+        },
+        {
+            id: 42,
+            name: "Pork with Panang Curry",
+            nimi: "Porsasta Panang Curryssa",
+            spice: 1,
+            price: 14.90
+        },
+        {
+            id: 43,
+            name: "Pork with Red Curry",
+            nimi: "Porsasta Punacurry Kookosmaidossa",
+            spice: 2,
+            price: 14.90
+        },
+        {
+            id: 44,
+            name: "Pork with Green Curry",
+            nimi: "Porsasta Vihercurryssa Kookosmaidossa",
+            spice: 2,
+            price: 14.90
+        },
+        {
+            id: 91, // sic again
+            name: "Pork with Sweet Chili Sauce",
+            nimi: "Porsasta Makeassa Chilitahnassa",
+            spice: 2,
+            price: 14.90
+        }
+    ],
+    beef: [
+        {
+            id: 45,
+            name: "Fried Beef with Vegetables",
+            nimi: "Paistettua Härkää ja Vihanneksia",
+            spice: 0,
+            price: 15.90
+        },
+        {
+            id: 46,
+            name: "Fried Beef with Garlic",
+            nimi: "Paistettua Härkää Valkosipulissa",
+            spice: 0,
+            price: 15.90
+        },
+        {
+            id: 47,
+            name: "Fried Beef with Cashew Nuts",
+            nimi: "Paistettua Härkää Cashewpähkinöillä",
+            spice: 0,
+            price: 15.90
+        },
+        {
+            id: 48,
+            name: "Fried Beef with Fresh Ginger",
+            nimi: "Paistettua Härkää Tuoreella Inkiväärillä",
+            spice: 1,
+            price: 15.90
+        },
+        {
+            id: 49,
+            name: "Fried Beef with Basil Leaves",
+            nimi: "Paistettua Härkkä ja Basilikaa",
+            spice: 2,
+            price: 15.90
+        },
+        {
+            id: 50,
+            name: "Fried Beef with Chili Sauce",
+            nimi: "Paistettua Härkkä Chilikastikkeessa",
+            spice: 1,
+            price: 15.90
+        },
+        {
+            id: 51,
+            name: "Fried Beef with panang Curry (Coconut Milk)",
+            nimi: "Härkkä Panang Curryssa (Kookosmaito)",
+            spice: 1,
+            price: 15.90
+        },
+        {
+            id: 52,
+            name: "Beef with Red Curry",
+            nimi: "Härkkä Punacurry Kookosmaidossa",
+            spice: 2,
+            price: 15.90
+        },
+        {
+            id: 53,
+            name: "Beef with Green Curry",
+            nimi: "Härkkä Vihercurry Kookosmaidossa",
+            spice: 2,
+            price: 15.90
+        }
+    ],
+    seafood: [
+        {
+            id: 54,
+            name: "Fried Fish with Garlic",
+            nimi: "Paistettua Kalaa Valkosipulissa",
+            spice: 0,
+            price: 16.00
+        },
+        {
+            id: 55,
+            name: "Fried Fish with Chili Sauce",
+            nimi: "Paistettua Kalaa Chilikastikkeessa (Tilapia)",
+            spice: 0,
+            price: 16.00
+        },
+        {
+            id: 56,
+            name: "Fried Fish with Fresh Ginger",
+            nimi: "Paistettua Kalaa Tuoreella Inkiväärillä",
+            spice: 1,
+            price: 16.00
+        },
+        {
+            id: 57,
+            name: "Fried Fish with Chuuchee Sauce",
+            nimi: "Paistettua Kalaa Chuuchee - Kastikkeessa",
+            spice: 2,
+            price: 16.00
+        },
+        {
+            id: 58,
+            name: "Fish Tom Yam Soup",
+            nimi: "Kalaa Tom Yam-keitto",
+            spice: 2,
+            price: 16.00
+        },
+        {
+            id: 59,
+            name: "Fried Squid with Garlic",
+            nimi: "Paistettua Mustekalaa Valkosipulissa",
+            spice: 0,
+            price: 16.00
+        },
+        {
+            id: 60,
+            name: "Fried Squid with Chili Sauce",
+            nimi: "Paistettua Mustekalaa Chilikastikkeessa",
+            spice: 2,
+            price: 16.00
+        },
+        {
+            id: 61,
+            name: "Seafood Tom Yam Soup (Squid and Prawns)",
+            nimi: "Merenherkut Tom Yam-keitto",
+            spice: 2,
+            price: 16.50
+        },
+        {
+            id: 62,
+            name: "Fried Prawns with Vegetables",
+            nimi: "Paistetut Jättikatkaravut ja Vihanneksia",
+            spice: 0,
+            price: 17.90
+        },
+        {
+            id: 63,
+            name: "Fried Prawns with Garlic",
+            nimi: "Paistetut Jättikatkaravut Valkosipulissa",
+            spice: 0,
+            price: 17.90
+        },
+        {
+            id: 64,
+            name: "Fried Prawns with Sweet and Sour Sauce",
+            nimi: "Paistetut Jättikatkaravut Hapanimeläkastikkeessa",
+            spice: 0,
+            price: 17.90
+        },
+        {
+            id: 65,
+            name: "Fried Prawns with Cashew Nuts",
+            nimi: "Paistetut Jättikatkaravut Cashewpähkinöillä",
+            spice: 1,
+            price: 17.90
+        },
+        {
+            id: 66,
+            name: "Fried Prawns with Chili Sauce",
+            nimi: "Paistetut Jättikatkaravut Chilikastikkeessa",
+            spice: 1,
+            price: 17.90
+        },
+        {
+            id: 67,
+            name: "Prawns with Panang Curry",
+            nimi: "Jättikatkaravut Panang Curryssa",
+            spice: 1,
+            price: 17.90
+        },
+        {
+            id: 68,
+            name: "Prawns with Red Curry",
+            nimi: "Jättikatkaravut Punacurry Kookosmaidossa",
+            spice: 2,
+            price: 17.90
+        },
+        {
+            id: 69,
+            name: "Prawns with Green Curry",
+            nimi: "Jättikatkaravut Vihercurry Kookosmaidossa",
+            spice: 2,
+            price: 17.90
+        },
+        {
+            id: 70,
+            name: "Prawns Tom Yam Soup",
+            nimi: "Jättikatkaravut Tom Yam-keitto",
+            spice: 2,
+            price: 17.90
+        }
+    ],
+    duck: [
+        {
+            id: 71,
+            name: "Duck with Red Curry",
+            nimi: "Ankaa Punacurry Kookosmaidossa",
+            spice: 2,
+            price: 17.00
+        },
+        {
+            id: 72,
+            name: "Duck Kaeng Kuo",
+            nimi: "Ankaa Kaeng Khuo",
+            spice: 2,
+            price: 17.00
+        },
+        {
+            id: 73,
+            name: "Fried Duck with Basil leaves",
+            nimi: "Paistettua Ankaa ja Basilikaa",
+            spice: 2,
+            price: 17.00
+        }
+    ],
+    friedRiceAndNoodles: [
+        {
+            id: 74,
+            name: "Fried Rice with Shrimp / Chicken / Pork / Beef",
+            nimi: "Paistettua Riisiä Katkarapujen / Kanan / Possun tai Härän Kanssa",
+            spice: 0,
+            price: 13.00
+        },
+        {
+            id: 75,
+            name: "Fried Noodles (Pad See Ew) with Shrimp / Chicken / Pork / Beef",
+            nimi: "Paistettua Nuudelia (Pad See Ew) Katkarapujen / Possun / Kanan tai Härän Kanssa",
+            spice: 0,
+            price: 14.90
+        },
+        {
+            id: 76,
+            name: "Fried Noodles (Pad Thai) with Shrimp / Chicken / Pork / Beef",
+            nimi: "Paistettua Nuudelia (Pad Thai) Katkarapujen / Kanan / Possun tai Härän Kanssa",
+            spice: 0,
+            price: 14.90
+        },
+        {
+            id: 77,
+            name: "Fried Noodles (Pad Kee Mao) with Shrimp / Chicken / Pork / Beef",
+            nimi: "Paistettua Nuudelia (Pad Kee Mao) Katkarapujen / Kanan / Possun tai Härän Kanssa",
+            spice: 2,
+            price: 14.90
+        }
+    ],
+    coldDishes: [
+        {
+            id: 78,
+            name: "Spicy Squid Salad",
+            nimi: "Tulinen Mustekalasalaatti",
+            spice: 2,
+            price: 16.00
+        },
+        {
+            id: 79,
+            name: "Spicy Beef Salad",
+            nimi: "Tulinen Häränihasalaatti",
+            spice: 2,
+            price: 16.00
+        },
+        {
+            id: 80,
+            name: "Spicy Seafood Salad",
+            nimi: "Tulinen Äyriäissalaatti",
+            spice: 2,
+            price: 16.00
+        },
+        {
+            id: 81,
+            name: "Spicy Chicken Salad Country Style",
+            nimi: "Tulinen Kanasalaatti Maalaistyyliin",
+            spice: 2,
+            price: 17.00
+        },
+        {
+            id: 82,
+            name: "Spicy Pork Salad Country Style",
+            nimi: "Tulinen Possusalaatti Maalaistyyliin",
+            spice: 2,
+            price: 17.00
+        },
+        {
+            id: 83,
+            name: "Spicy Beef Salad Country Style",
+            nimi: "Tulinen Häränihasalaatti Maalaistyyliin",
+            spice: 2,
+            price: 17.00
+        }
+    ],
+    dessert: [
+        {
+            id: 85,
+            name: "Deep Fried Banana and ice Cream",
+            nimi: "Friteerattu Banaani ja Jäätelö",
+            spice: 0,
+            price: 6.90
+        },
+        {
+            id: 86,
+            name: "Pancake and Ice Cream",
+            nimi: "Ohukaisia ja jäätelöä",
+            spice: 0,
+            price: 6.90
+        },
+        {
+            id: 87,
+            name: "Banana and Coconut Milk",
+            nimi: "Banaania ja Kookosta (Lämmin)",
+            spice: 0,
+            price: 6.90
+        },
+        {
+            id: 88,
+            name: "Coffee",
+            nimi: "Kahvi",
+            spice: 0,
+            price: 2.00
+        },
+        {
+            id: 89,
+            name: "Tea",
+            nimi: "Tee",
+            spice: 0,
+            price: 2.00
+        }
+    ]
+}
